fe9ec0394d19084a5230a25d9e774280be6a2006
dmik <dmik@test>
2018-02-09 12:04:21 +0200

    First Commit

---
+++ GitOperations/GitOperations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstVariable = 12;
+            var secondVariable = 42;
+        }
+    }
+}
---
+++ GitOperations/GitOperations/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitOperations
+{
+    class TestClass
+    {
+        string testString = "test";
+        int testInt = 42;
+    }
+}
