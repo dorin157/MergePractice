57e08c982c66ba1118e5ea6bd7c69ba5f9c84dbb
Микайло Дорін <leowingoo@gmail.com>
2019-04-10 13:52:14 +0300

    Update TestClass.cs

--- GitOperations/GitOperations/TestClass.cs
+++ GitOperations/GitOperations/TestClass.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,7 @@ namespace GitOperations
     {
         string testString = "test2";
         int testInt = 42;
-        int uno = 1;
+        int uno = 2;
         int reb = 3;
     }
 }
